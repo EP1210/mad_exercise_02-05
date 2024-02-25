f19be06a7aff9c2f3042a24efaab1cb1d478e720
Erik <erik.pfandler@stud.fh-campuswien.ac.at>
2024-02-25 19:21:05 +0100

    Improvements on composable function MovieRow

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -21,11 +23,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
 
 class MainActivity : ComponentActivity() {
@@ -38,51 +38,54 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Card {
-                        Image(painter = painterResource(id = R.drawable.movie_image), contentDescription = "placeholder_image")
-                    }
+                    MovieRow()
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
 fun MovieRow() {
-    Column {
-            Card(
-                shape = RectangleShape,
-                modifier = Modifier
-                    .padding(10.dp)
-            ) {
-                Box {
-                    Image(
-                        painter = painterResource(id = R.drawable.movie_image),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .padding(all = 5.dp)
-                            .clip(shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
-                    )
-                    Icon(
-                        imageVector = Icons.Default.FavoriteBorder,
-                        contentDescription = null,
-                        modifier = Modifier
-                            .align(alignment = Alignment.TopEnd)
-                            .padding(all = 20.dp)
-                    )
-                }
-                Row {
-                    Text(
-                        text = "Avatar",
-                        modifier = Modifier
-                            .padding(start = 10.dp, bottom = 15.dp)
-                    )
-                    Icon(
-                        imageVector = Icons.Default.KeyboardArrowUp,
-                        contentDescription = null,
-                    )
-                }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 10.dp)
+    ) {
+
+        Card(
+            shape = RoundedCornerShape(size = 20.dp)
+        ) {
+
+            Box {
+                Image(
+                    painter = painterResource(id = R.drawable.movie_image),
+                    contentDescription = null
+                )
+                Icon(
+                    imageVector = Icons.Default.FavoriteBorder,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(alignment = Alignment.TopEnd)
+                        .padding(all = 20.dp)
+                )
             }
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(all = 8.dp)
+            ) {
+                Text(
+                    text = "Avatar",
+                    fontSize = 18.sp
+                )
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowUp,
+                    contentDescription = null
+                )
+            }
+        }
     }
 }
